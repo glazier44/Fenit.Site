50dafafa928e2609f556ee1a8d991d13348d605c
Tomek Kwiek <kwiek_tomasz@hotmail.com>
2018-10-19 19:09:16 +0200

    renema

---
+++ Fenite.Site.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Optimization;
+
+namespace FeniteSite.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/angular")
+                .Include(
+                    "~/Scripts/angular/inline.*",
+                    "~/Scripts/angular/polyfills.*",
+                    "~/Scripts/angular/scripts.*",
+                    "~/Scripts/angular/styles.*",
+                    "~/Scripts/angular/vendor.*",
+                    "~/Scripts/angular/main.*"));
+
+
+
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+        }
+    }
+}
--- Fenite.Site.Web/src/app/app.component.spec.ts
+++ Fenite.Site.Web/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'gitHouseApp'`, async(() => {
+  it(`should have as title 'FeniteSite'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gitHouseApp');
+    expect(app.title).toEqual('FeniteSite');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to gitHouseApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to FeniteSite!');
   }));
 });
